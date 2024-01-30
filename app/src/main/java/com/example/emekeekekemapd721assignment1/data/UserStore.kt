7e08f7ca123ec649b3f58bbced5b2d1f701e7eb5
kingmeky <emeka.andrew.ekeke@gmail.com>
2024-01-30 15:07:19 -0500

    first commit

---
+++ app/src/main/java/com/example/emekeekekemapd721assignment1/data/UserStore.kt
@@ -0,0 +1,7 @@
+package com.example.emekeekekemapd721assignment1.data
+
+import android.content.Context
+
+class UserStore(context: Context) {
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EmekeEkekeMAPD721Assignment1"
+include(":app")
+ 

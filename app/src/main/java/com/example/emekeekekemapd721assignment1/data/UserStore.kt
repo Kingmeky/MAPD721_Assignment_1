ed5a42cc428ecc800b1d42f80e18382679ffa46b
kingmeky <emeka.andrew.ekeke@gmail.com>
2024-01-31 13:28:40 -0500

    fourth commit

--- app/src/main/java/com/example/emekeekekemapd721assignment1/data/UserStore.kt
+++ app/src/main/java/com/example/emekeekekemapd721assignment1/data/UserStore.kt
@@ -3,6 +3,7 @@ package com.example.emekeekekemapd721assignment1.data
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
@@ -23,4 +24,21 @@ class UserStore(private val context: Context) {
         "Name: $name, Userid: $userid, Email: $email"
     }.onStart { emit("") }
 
+    suspend fun saveData(name: String, userid: String, email: String) {
+        context.dataStore.edit { preferences ->
+            preferences[NAME_KEY] = name
+            preferences[USERID_KEY] = userid
+            preferences[EMAIL_KEY] = email
+        }
     }
+
+    suspend fun clearData() {
+        context.dataStore.edit { preferences ->
+            preferences.remove(NAME_KEY)
+            preferences.remove(USERID_KEY)
+            preferences.remove(EMAIL_KEY)
+        }
+    }
+}
+
+
